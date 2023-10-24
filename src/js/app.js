53e2eafc13892ccd64c31d3e981e29b44332c3a4
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-10-24 23:35:03 +0530

    javaScript begin

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,26 @@
 /**
- * @copyright codewithsadee 2023
+ * @copyright sukhmeet 2023
  */
 
 'use strict';
+
+/**
+  * Module Import
+  */
+
+import { addEventOnElements } from "./utils.js";
+
+/**
+  * Toggle sidebar in small screen
+  */
+
+const /** {HTML Element} */ $sidebar = document.querySelector('[ data-sidebar]');
+
+const /** {Array<HTML Element>} */ $sidebarTogglers = document.querySelectorAll('[ data-sidebar-toggler]');
+
+const /** {HTML Element} */ $overlay = document.querySelector('[ data-sidebar-overlay]');
+
+addEventOnElements($sidebarTogglers, 'click', function(){
+    $sidebar.classList.toggle('active');
+    $overlay.classList.toggle('active');
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,20 @@
  */
 
 'use strict';
+
+/**
+ * 
+ * @param {Array<HTMLElement>} $elements - An array of DOM elements to attach the event listener to.
+ * @param {String} eventType - The type of event to listen for (e.g., 'click', 'mouseover').
+ * @param {Function} callback - The function to be executed when the event occurs.
+ */
+
+
+const addEventOnElements = function ($elements, eventType ,callback) {
+    console.log($elements);
+    $elements.forEach($elements => $element.addEventListener(eventType,callback));
+}
+
+export {
+    addEventOnElements
+}
